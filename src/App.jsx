fb0d0ac62bbd1d122d853c26dba09acb21d910e7
Nicolas Castejon <nicolas.castejon@protonmail.com>
2023-03-18 20:50:07 +0100

    Add 3 examples + a bit of UI

--- src/App.jsx
+++ src/App.jsx
@@ -33,29 +33,20 @@ const App = () => {
       </div>
       <div className={styles.sidebar}>
         {/* Add example buttons here */}
-        <button>Example 1</button>
-        <button>Example 2</button>
-        <button>Example 3</button>
-        {/* <option value="1BNA">1BNA - Simple 1</option>
-                <option value="2MHR">2MHR - Simple 2</option>
-                <option value="1CRN">1CRN - Simple 3</option>
-                <option value="1MBA">1MBA - Moderately Complex</option>
-                <option value="1F88">1F88 - Complex</option> */}
+        <div>Try one example</div>
+        <button onClick={() => setSelectedPdbId('1CRN')}>1CRN</button>
+        <button onClick={() => setSelectedPdbId('1F88')}>1F88</button>
+        <button onClick={() => setSelectedPdbId('9ANT')}>9ANT</button>
+
       </div>
       <div className={styles.content}>
-        {/* Add your ProteinRibbon canvas here */}
-        <div id="protein-ribbon-canvas">
           <ProteinRibbon
             key={selectedPdbId}
             pdbId={selectedPdbId}
             onResidueSelection={handleResidueSelection}
           />
-        </div>
-
         {/* Add the QueryProtein component here */}
         <div className={styles.queryContainer}>
-          <select value={selectedPdbId} onChange={handleChange}>
-          </select>
           <QueryProtein onPDBSelection={setSelectedPdbId} />
         </div>
       </div>
