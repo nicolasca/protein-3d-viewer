504bf1c15c8d59b3580f9beb16cdcebc7d105ccb
Nicolas Castejon <nicolas.castejon@protonmail.com>
2023-03-19 14:26:48 +0100

    Some ui + content

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,18 +9,11 @@ export const Home = ({onChangeLocation}) => {
         <div className={styles.Container}>
             {/* Add two images with one 75% of width et 25% for the other */}
             <div className={styles.Left}>
-                {/* <div className={styles.Title}>
-                    <span>
-                        Explore the Building Blocks of Life with Our Interactive 3D Protein Viewer!
-                    </span>
-                    <button>
-                    </button>
-                </div> */}
             </div>
             <div className={styles.Right}>
                 <div className={styles.CallToAction}>
                     <span className={styles.Button} onClick={() => onChangeLocation("visualizer")}>Unveil the Mystery</span>
-                    <span className={styles.Text}>Proteins: because understanding the intricacies of molecules is far less perplexing than deciphering human emotions</span>
+                    <span className={styles.Text}>The perfect pet for science nerds. No poop scooping required.</span>
                     <img src={proteinUrl} />
                 </div>
             </div>
